b09f50c623ba5544d063703d17566b1f4ecd764b
ken pu <kenpuca@gmailcom>
2015-04-06 17:07:37 -0400

    actors

---
+++ sandbox/actors.scala
@@ -0,0 +1,26 @@
+import scala.actors.Actor
+import scala.actors.Actor._
+
+class Echoer(i: Int) extends Actor {
+    def act() {
+        val id = Thread.currentThread.getId()
+        loop {
+            react {
+                case x:Int => printf("Echo(%d, %d): %s\n", i, id, x)
+                case x:Any => printf("Echo(%d, %d): unk %s\n", i, id, x)
+            }
+        }
+    }
+}
+
+val echoers = (1 to 100).map(i => new Echoer(i))
+
+echoers.foreach(_.start)
+
+for(e <- echoers) {
+    e ! 1
+    e ! 2
+    e ! "hello"
+}
+
+println("All done")
